a393f1a033ceccd8a7b95166db07683a74bad903
Nrabyan-17 <naufalrizkiabyan@gmail.com>
2025-04-17 13:33:06 +0700

    ubah referensi

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ include('session.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Management System - Dashboard</title>
-    
+
     <!-- Bootstrap core css -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- Feather JS for icons -->
